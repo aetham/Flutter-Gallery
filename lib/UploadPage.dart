97e767aa65f40e1351dee1865556c153fb9a2cc0
aetham <henri206@hotmail.com>
2021-05-27 10:53:05 +0300

    Added Page to upload files

---
+++ lib/UploadPage.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gallery/widget/button_widget.dart';
+import 'package:path/path.dart';
+import 'api/firebase_api.dart';
+
+class UploadPage extends StatefulWidget {
+  static final String title = 'Firebase Upload';
+  @override
+  _UploadPageState createState() => _UploadPageState();
+}
+
+class _UploadPageState extends State<UploadPage> {
+  UploadTask? task;
+  File? file;
+
+  @override
+  Widget build(BuildContext context) {
+    final fileName = file != null ? basename(file!.path) : 'No File Selected';
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ButtonWidget(
+                text: 'Select File',
+                icon: Icons.attach_file,
+                onClicked: selectFile,
+              ),
+              SizedBox(height: 8),
+              Text(
+                fileName,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 48),
+              ButtonWidget(
+                text: 'Upload File',
+                icon: Icons.cloud_upload_outlined,
+                onClicked: uploadFile,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+    if (result == null) return;
+    final path = result.files.single.path!;
+    setState(() => file = File(path));
+  }
+
+  Future uploadFile() async {
+    if (file == null) return;
+    final fileName = basename(file!.path);
+    final destination = 'files/$fileName';
+    task = FirebaseApi.uploadFile(destination, file!);
+  }
+}
