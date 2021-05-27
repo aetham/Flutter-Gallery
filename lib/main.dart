e2256a4501b6771138e511c419f26171e693229f
aetham <henri206@hotmail.com>
2021-05-27 10:52:31 +0300

    Initalizing firebase

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'MainPage.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gallery!',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MainPage(),
+    );
+  }
+}
